dbfd69bc1bdf70b877aad144308e0fc8696c0674
Gino Rossi <gp.rossijr@gmail.com>
2021-02-04 03:37:42 +0000

    added docs on 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ *main - prints "Programming is like building a multilingual puzzle
+ *
+ *Description:it outputs the quote in the command known as "puts"
+ *Return: it returns 0
+ */
 main()
 {
 puts("\"Programming is like building a multilingual puzzle");
