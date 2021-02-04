024b48008b6d6911167ed8c4ed96f5a7a0561883
Gino Rossi <gp.rossijr@gmail.com>
2021-02-04 04:07:12 +0000

    added 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  *Description:it outputs the quote in the command known as "puts"
  *Return: it returns 0
  */
-main()
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
