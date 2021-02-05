70ae95523f58d65b91f7c7f043bacc4a8eefcdb0
Gino Rossi <gp.rossijr@gmail.com>
2021-02-05 01:56:23 +0000

    added 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - prints reverse alphabet
+ *
+ *Description: will print the lower case alphabet in reverse
+ *Return: 0
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i-- )
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
