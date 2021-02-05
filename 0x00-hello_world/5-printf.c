a802a31042cfba43f0906ae868e3f9cc5ea6fbf5
Gino Rossi <gp.rossijr@gmail.com>
2021-02-05 15:11:08 +0000

    added 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main -prints "with proper grammar, but the outcome is a piece of art,
+ *
+ *Description:it outputs the quote in the command known as "printf"
+ *Return: it returns 0
+ */
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
