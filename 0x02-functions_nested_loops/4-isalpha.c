9779f2738c6accc5d36095cb1828a9b9ca997bca
Gino Rossi <gp.rossijr@gmail.com>
2021-02-10 14:52:22 +0000

    added 4, updated header

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - determines if character is alphabetic
+ * @c: entered character
+ *
+ * Return: true if entered char is alphabetic, false otherwise
+ */
+int _isalpha(int c)
+{
+	int result;
+
+	result = 0;
+	if (c >= 97 && c <= 122)
+	{
+		result = 1;
+	}
+	else if(c >= 65 && c <= 90)
+	{
+		result = 1;
+	}
+	return (result);
+}
