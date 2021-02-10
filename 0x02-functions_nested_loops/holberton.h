939915f4fff28deff9f1631dd69653ac23561817
Gino Rossi <gp.rossijr@gmail.com>
2021-02-10 00:59:11 +0000

    update house function

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -24,7 +24,16 @@ int _putchar(char c)
  */
 int house(void)
 {
-	printf("Holberton\n");
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 	return (0);
 }
 
