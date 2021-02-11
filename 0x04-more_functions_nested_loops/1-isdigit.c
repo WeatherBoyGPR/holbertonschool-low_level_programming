be4388df07858fa458a166a08131f0d2814476b5
Gino Rossi <gp.rossijr@gmail.com>
2021-02-11 17:46:36 +0000

    added 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - determines if c is a digit
+ * @c: is the integer that is operated on
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	int ManicDepression;
+
+	ManicDepression = (0);
+	if (c >= 48 && c <= 57)
+	{
+		ManicDepression = (1);
+	}
+	return (ManicDepression);
+}
