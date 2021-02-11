f23027b52ae6805120d9e2f5dfe7dfbc1c32cc8e
Gino Rossi <gp.rossijr@gmail.com>
2021-02-11 17:53:18 +0000

    added 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies the two entered integers
+ *@a: the first integer to be multiplied
+ *@b: the second integer to be multiplied
+ *
+ * Return: the product f the two integers
+ */
+int mul(int a, int b)
+{
+	int Santana = (a * b);
+
+	return (Santana);
+}
