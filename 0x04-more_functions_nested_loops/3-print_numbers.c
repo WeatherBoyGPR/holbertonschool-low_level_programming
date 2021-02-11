2a52cb200b29cd259f626ad8aa19a23f071eaf0b
Gino Rossi <gp.rossijr@gmail.com>
2021-02-11 18:21:47 +0000

    added 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - prints the numbers 0 through 9
+ *
+ * Return: doesn't
+ */
+void print_numbers(void)
+{
+	int epitaph = 48;
+
+	for (epitaph == 48; epitaph <= 57; epitaph++)
+	{
+		_putchar(epitaph);
+	}
+	_putchar('\n');
+}
