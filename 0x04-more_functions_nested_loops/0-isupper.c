71388f299664351712a3e0b77e87413e421bcb98
Gino Rossi <gp.rossijr@gmail.com>
2021-02-11 17:11:01 +0000

    added 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _isupper - determines if c is uppercase
+ * @c: is the integer that is operated on
+ *
+ * Return: afdafsdf.
+ */
+int _isupper(int c)
+{
+	int magentamagenta;
+
+	magentamagenta = (0);
+	if (c >= 65 && c <= 90)
+	{
+		magentamagenta = (1);
+	}
+	return (magentamagenta);
+}
