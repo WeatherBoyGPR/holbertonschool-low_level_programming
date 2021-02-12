e5ff4f2dc566df2989396cf6462f5dfb866441ce
Gino Rossi <gp.rossijr@gmail.com>
2021-02-12 15:23:51 +0000

    added 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ * main - goes through fizzbuzz test
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int aqualung = (1);
+	int whiteroom = (0);
+	int lifeonmars = (0);
+
+	for (aqualung = 1; aqualung <= 100; aqualung++)
+	{
+		whiteroom = (aqualung % 3);
+		lifeonmars = (aqualung % 5);
+		if (whiteroom != 0 && lifeonmars != 0)
+		{
+			printf("%i", aqualung);
+		}
+		else if (whiteroom == 0 && lifeonmars != 0)
+		{
+			printf("%s", "Fizz");
+		}
+		else if (whiteroom != 0 && lifeonmars == 0)
+		{
+			printf("%s", "Buzz");
+		}
+		else if (whiteroom == 0 && lifeonmars == 0)
+		{
+			printf("%s", "FizzBuzz");
+		}
+		if (aqualung < 100)
+		{
+			putchar(' ');
+		}
+	}
+	return (0);
+}
