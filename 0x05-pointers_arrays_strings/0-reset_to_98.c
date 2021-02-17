22f155699c6383e8d2d1373c9f5fe1f385603f76
Gino Rossi <gp.rossijr@gmail.com>
2021-02-17 03:27:50 +0000

    adding 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
-#include <holberton.h>
+#include "holberton.h"
 
 /**
  * reset_to_98 - update the value of an integer to 98
- * @n; the pointer that will be used to update the integer
+ * @*n; the pointer that will be used to update the integer
  *
  * Return: None.
  */
