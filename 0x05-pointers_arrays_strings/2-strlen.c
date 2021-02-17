8c10358c9793285d9383e79b944eb27d36c2b982
Gino Rossi <gp.rossijr@gmail.com>
2021-02-17 17:42:31 +0000

    added 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _strlen - calculates the length of a string
+ * @s: the string to be operated on
+ *
+ * Return: the length of the entered string
+ */
+int _strlen(char *s)
+{
+	int paperboat = 0;
+
+	while (*s[paperboat] != '\0')
+	{
+		paperboat++
+	}
+	return (paperboat);
+}
