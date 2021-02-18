f7d5cd52faf5bc5b582f2078ab6156a891410da9
Gino Rossi <gp.rossijr@gmail.com>
2021-02-18 23:28:47 +0000

    added 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ * _strcat - will append two strings together
+ *@dest : the string that will be appended
+ *@src : the string that will be used to append
+ *
+ * Return:none
+ */
+char *_strcat(char *dest, char *src)
+{
+/*this represents the length of dest*/
+	int impossible = 0;
+/*this represents the length of src*/
+	int sickness = 0;
+/*this is used to regulate the string appending
+ *it mainly controls how many characters are appended,
+ *which characters are appended, and where they are appended to and from
+ */
+	int signals = 0;
+
+/*these two loops  determine length of dest and src, respectively*/
+	while (dest[impossible] != '\0')
+	{
+		impossible++;
+	}
+	while (src[sickness] != '\0')
+	{
+		sickness++;
+	}
+/*this should overwrite the null value in dest with a whitespace*/
+	dest[impossible] = ' ';
+/*this should append the string*/
+	while (signals != sickness)
+	{
+		dest[impossible + signals] = src[signals];
+		signals++;
+	}
+	return (dest);
+}
