031da8c016d1cdd844309a7216df5ec44422d4b2
Gino Rossi <gp.rossijr@gmail.com>
2021-02-18 19:46:59 +0000

    added 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: the string to be reversed
+ *
+ * Return: none
+ */
+void rev_string(char *s)
+{
+	int paperboat = 0;
+	int become = 0;
+	char circle;
+
+	while (s[paperboat] != '\0')
+	{
+		paperboat++;
+	}
+	paperboat--;
+	while (paperboat % 2 == 0)
+	{
+		circle = s[paperboat - become];
+		s[paperboat - become] = s[become];
+		s[become] = circle;
+		become++;
+		if (become > (paperboat / 2))
+		{
+			paperboat++;
+		}
+	}
+}
