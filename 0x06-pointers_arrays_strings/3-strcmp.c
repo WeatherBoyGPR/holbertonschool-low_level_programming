8e3a03f3c88591bb510f4c8f689ca74dab7f05e6
Gino Rossi <gp.rossijr@gmail.com>
2021-02-19 17:49:43 +0000

    added 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+include "holberton.h"
+
+/**
+ *_strcmp - compares two strings
+ *@s1 : first string to be compared
+ *@s2 : second string to be compared
+ *
+ *Return: difference
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int index = 0;
+	int difference = 0;
+
+	do {
+		difference = s1[index] - s2[index];
+		if (s1[index] == '\0' && s2[index] == '\0')
+		{
+			break;
+		}
+		index++;
+	} while (s1[index - 1] == s2[index - 1] || difference == 0);
+	return (difference);
+}
