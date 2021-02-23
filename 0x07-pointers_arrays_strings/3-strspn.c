508d6447a9328112d0cc0ba8b76afd80125fee8a
Gino Rossi <gp.rossijr@gmail.com>
2021-02-23 03:28:53 +0000

    Added 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ *_strspn - obtains length of a prefix substring
+ *@s: the string that will be scanned through
+ *@accept: the string which contains the bytes to be searched for
+ *
+ *Return: returns number of bytes in the inital segment of s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int index1 = 0;
+	int index2;
+	unsigned int result = 0;
+
+	while (s[index1] != 44)
+	{
+		index2 = 0;
+		while (accept[index2] != '\0')
+		{
+			if (s[index1] == accept[index2])
+			{
+				result++;
+			}
+			index2++;
+		}
+		index1++;
+	}
+	return (result);
+}
