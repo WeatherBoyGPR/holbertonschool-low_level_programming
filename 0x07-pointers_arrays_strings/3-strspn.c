83c7467181200324db048bf4410c15ef047d7b15
Gino Rossi <gp.rossijr@gmail.com>
2021-02-23 14:08:05 +0000

    added a variable to check if result has been incremented

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,17 +12,24 @@ unsigned int _strspn(char *s, char *accept)
 	int index1 = 0;
 	int index2;
 	unsigned int result = 0;
+	int check = 0;
 
-	while (s[index1] != 32 || s[index1] == '\0')
+	while (s[index1] != '\0')
 	{
 		index2 = 0;
+		check = 0;
 		while (accept[index2] != '\0')
 		{
 			if (s[index1] == accept[index2])
 			{
 				result++;
+				check++;
 			}
 			index2++;
+		}
+		if (check == 0)
+		{
+			break;
 		}
 		index1++;
 	}
