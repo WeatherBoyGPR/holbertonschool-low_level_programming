f09fe808bd342242eb2faa8cb2064d44816a834e
Gino Rossi <gp.rossijr@gmail.com>
2021-02-24 20:21:17 +0000

    added 0

---
+++ 0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+*_puts_recursion - prints a string
+*@s -  string to be printed
+*
+*Return: doesn't
+*/
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	if (*s != '\0')
+	{
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
+
