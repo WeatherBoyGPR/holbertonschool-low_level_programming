535530082d95909af4610cc3b77915f8e71b9591
Gino Rossi <gp.rossijr@gmail.com>
2021-02-24 02:35:49 +0000

    added 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+*print_chessboard - prints the chessboard
+*@a: is the chessboard
+*
+*Return: doesn't
+*/
+void print_chessboard(char (*a)[8])
+{
+	int indexX, indexY;
+
+	for (indexY = 0; indexY < 8; indexY++)
+	{
+		for (indexX = 0; indexX < 8; indeX++)
+		{
+			_putchar(a[indexX][indexY]);
+		}
+		_putchar('\n');
+	}
+}
