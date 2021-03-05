805f96a9509a61839d7032bb80c77c1060dc9733
Gino Rossi <gp.rossijr@gmail.com>
2021-03-05 00:02:18 +0000

    added 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include "stdlib.h"
+
+/**
+*malloc_checked -allocates memory
+*@b: amount of space to be allocated
+*
+*Return: pointer to allocated space
+*/
+void *malloc_checked(unsigned int b)
+{
+	int *pointer;
+
+	pointer = malloc(b);
+
+	if (pointer == NULL)
+	exit(98);
+
+	else
+	return (pointer);
+}
