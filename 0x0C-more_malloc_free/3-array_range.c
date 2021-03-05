2915fc3f248b5632474ecdc1f064be5bf0bb7438
Gino Rossi <gp.rossijr@gmail.com>
2021-03-05 19:39:20 +0000

    added 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+*array_range - will create an array to contain min, max, and numbers inbetween
+*@max: largest number in the array
+*@min: smallest number in the array
+*
+*Return: pointer to the created array
+*/
+int *array_range(int max, int min)
+{
+	int arraysize, holder, index;
+	int *newpoint;
+
+	if (min > max)
+	return (NULL);
+
+	arraysize = (max - min + 1);
+
+	newpoint = malloc(arraysize * sizeof(int));
+	if (newpoint == NULL)
+	return (NULL);
+
+	holder = min;
+
+	for (index = 0; index < arraysize; index++)
+	{
+		newpoint[index] = holder;
+		holder++;
+	}
+
+	return (newpoint);
+}
