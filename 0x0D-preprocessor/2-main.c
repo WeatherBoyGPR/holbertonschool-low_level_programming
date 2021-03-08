b79e37bb5c1377b55e0080460a76ba345047a187
Gino Rossi <gp.rossijr@gmail.com>
2021-03-08 18:20:08 +0000

    added 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,22 @@
-#include <stdio.h>
+#include "holberton.h"
 
-void main(void)
+/**
+*main - will print the name of the file it is in
+*
+*Return: doesn't
+*/
+int main()
 {
-	printf("%s\n" , __FILE__);
+	int incre = 0;
+	char *point;
+
+	point = __FILE__;
+
+	while (point[incre] != '\0')
+	{
+		_putchar(point[incre]);
+		incre++;
+	}
+	_putchar('\n');
+	return (0);
 }
