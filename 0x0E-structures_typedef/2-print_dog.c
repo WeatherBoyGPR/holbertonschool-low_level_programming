58fe23f3682c103826c7dc7028a96ad3b10584b7
Gino Rossi <gp.rossijr@gmail.com>
2021-03-10 02:25:12 +0000

    added 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,30 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*print_dog - will disclose "the dog"
+*@d: "the dog"
+*
+*Return: it doesn't
+*/
+void print_dog(struct dog *d)
+{
+	if (d == 0)
+	return;
+
+	if (d->name != NULL)
+		printf("Name: %s\n", d->name);
+	else
+		printf("Name: (nil)\n");
+
+	if (d->age > 0 && d->age != 0)
+		printf("Age: %f\n", d->age);
+	else
+		printf("Age: (nil)\n");
+
+	if (d->owner != NULL)
+		printf("Owner: %s\n", d->owner);
+	else
+		printf("Owner: (nil)\n");
+}
