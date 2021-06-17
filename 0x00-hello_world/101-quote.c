001a739f563662b562dcdc91e63289ba007026c7
Gino Rossi <gp.rossijr@gmail.com>
2021-06-17 18:16:36 +0000

    added 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,13 @@
 #include <unistd.h>
 
-int main()
+/**
+ * main - Will print a quote
+ *
+ * Return: the return value of write
+ */
+int main(void)
 {
-	return (write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59));
+	char *q = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	return (write(2, q, 59));
 }
