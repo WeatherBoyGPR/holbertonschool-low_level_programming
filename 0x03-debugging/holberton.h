c9b1d7f8f57eda682ac4aec0dbb6af6c3e3bfa56
Gino Rossi <gp.rossijr@gmail.com>
2021-06-25 14:18:15 +0000

    updated holberton.h

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,6 +1,8 @@
 #ifndef TURNTOSTONE
 #define TURNTOSTONE
 
+#include <stdio.h>
+
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
