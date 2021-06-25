28ee90a6d411ae0753bb8f8914fba718c5ecefaf
Gino Rossi <gp.rossijr@gmail.com>
2021-06-25 02:50:30 +0000

    updated 6-print_line to comply with betty

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
 
+/**
+ * Will print a line of the length specified by length
+ * @n: length of the line to be printed
+ *
+ * Return: doesn't
+ */
 void print_line(int n)
 {
 	int i;
