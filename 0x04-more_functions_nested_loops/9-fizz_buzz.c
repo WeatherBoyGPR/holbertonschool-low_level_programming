2ec72a409a850aebcc9732e5c48a9439b6776fd3
Gino Rossi <gp.rossijr@gmail.com>
2021-06-25 03:27:45 +0000

    Added 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - will perform fizzbuzz
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 99; num++)
+	{
+		if (!(num % 3))
+			printf("Fizz ");
+		if (!(num % 5))
+			printf("Buzz ");
+		if (!(num % 3) && !(num % 5))
+			printf("FizzBuzz");
+		if (num % 3 && num % 5)
+			printf("%d ", num);
+	}
+	printf("100\n");
+
+	return (0);
+}
