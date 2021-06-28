6ebbdc04a867c66ac061e0b3dae7d9c2721ed998
Gino Rossi <gp.rossijr@gmail.com>
2021-06-28 14:18:28 +0000

    added 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - will reset the value pointed to by n to 98
+ * @n: a pointer to an integer
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	if (n)
+		*n = 98;
+}
