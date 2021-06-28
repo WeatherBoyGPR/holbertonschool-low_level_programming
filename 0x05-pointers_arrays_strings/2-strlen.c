1d14ee683f1cf3ff1c03f6877853105101b034c4
Gino Rossi <gp.rossijr@gmail.com>
2021-06-28 14:44:46 +0000

    updated 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -8,12 +8,12 @@
  */
 int _strlen(char *s)
 {
-	int i;
+	int i = 0;
 
 	if (s && *s)
 	{
-		for (i = 0; *(s + i) != '\0'; i++)
-			continue;
+		while (*(s + i) != '\0')
+			i++;
 	}
 
 	return (i);
