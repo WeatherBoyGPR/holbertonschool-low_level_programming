968b5d9bce07608cd503eebefae4adae3377eb41
Gino Rossi <gp.rossijr@gmail.com>
2021-06-28 19:05:10 +0000

    updated 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,16 +9,18 @@
 void puts_half(char *str)
 {
 	int length;
-	int i;
+	int i, n;
 
 	for (length = 0; str[length] != '\0'; length++)
 		continue;
+
+	n = length / 2;
 
 	if (length % 2)
 		length++;
 	length /= 2;
 
-	for (i = length; length; i++, length--)
+	for (i = length; n; i++, n--)
 		_putchar(str[i]);
 	_putchar('\n');
 }
