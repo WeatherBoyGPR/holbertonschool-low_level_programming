5c83341f76fabd10bcb0ba0e2c9f55de8cc4ae63
Gino Rossi <gp.rossijr@gmail.com>
2021-07-01 18:08:04 +0000

    added 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - will copy a string to a destination buffer
+ * @dest: destination string
+ * @src: string to be copied from
+ * @n: maximum number of bytes to be copied
+ *
+ * Return: pointer to dest string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	int l = 1;
+
+	for (i = 0; *(src + i) != '\0'; i++)
+		l++;
+
+	for (i = 0; n && l; i++)
+	{
+		*(dest + i) = src[i];
+		l--;
+		n--;
+	}
+
+	return (dest);
+}
