f99007c049e9f8c384a97cfe4849088e823b3c19
Gino Rossi <gp.rossijr@gmail.com>
2021-07-07 03:07:00 +0000

    added 100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * set_string - sets the value of a pointer
+ * @s: pointer to be modified
+ * @to: string to set s to
+ *
+ * Return: doesn't
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
