36b1a2ad0a4a22517cc0e77b7f56036f19fc874a
Gino Rossi <gp.rossijr@gmail.com>
2021-07-08 18:39:24 +0000

    updated header file holberton.h

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -10,6 +10,7 @@ int _pow_recursion(int x, int y);
 int process(int n, int tar);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int primo(int n , int tar);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
