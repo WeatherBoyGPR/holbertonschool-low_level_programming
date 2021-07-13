87fdbcaf6a78d92a87ea611377d0c162c19653a7
Gino Rossi <gp.rossijr@gmail.com>
2021-07-13 23:40:46 +0000

    updated 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,32 +10,36 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *str;
-	int l = 0, i;
+	int l = 0, i = 0;
 
-	if (!s1 || !s2)
-		return (NULL);
-
-	for (i = 0; *(s1 + i) != '\0'; i++)
-		l++;
-	for (i = 0; *(s2 + i) != '\0'; i++)
-		l++;
+	if (s1)
+		for (i = 0; *(s1 + i) != '\0'; i++)
+			l++;
+	if (s2)
+		for (i = 0; *(s2 + i) != '\0'; i++)
+			l++;
 	l++;
 
 	str = malloc(sizeof(char) * l);
 
 	if (!str)
 		return (NULL);
-	for (i = 0; *(s1) != '\0'; i++)
-	{
-		*(str + i) = *(s1);
-		s1++;
-	}
-	while (*s2 != '\0')
-	{
-		*(str + i) = *(s2);
-		s2++;
-		i++;
-	}
+
+	i = 0;
+	if (s1)
+		while (*(s1) != '\0' && s1)
+		{
+			*(str + i) = *(s1);
+			s1++;
+			i++;
+		}
+	if (s2)
+		while (*s2 != '\0' && s2)
+		{
+			*(str + i) = *(s2);
+			s2++;
+			i++;
+		}
 	*(str + i) = '\0';
 
 	return (str);
