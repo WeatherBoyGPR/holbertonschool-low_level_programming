7daeab32b7015cf50d85f0ca90551a9946f1a251
Gino Rossi <gp.rossijr@gmail.com>
2021-07-15 20:30:41 +0000

    added 0-malloc_checked.c, updated holberton.h

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * malloc_checked - will allocate memory with malloc
+ * @b: bytes of memory to allocate
+ *
+ * Return: pointer to allocated memory upon success
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *res = malloc(b);
+	if (res == NULL)
+		exit(98);
+	return (res);
+}
