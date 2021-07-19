992ebf0b3e8af37cbad20ce7dc4664329a59174f
Gino Rossi <gp.rossijr@gmail.com>
2021-07-19 23:02:59 +0000

    added 2-print_dog.c, updated dog.h header file

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdio.h>
+
+void print_dog(struct dog *d)
+{
+	if (!d)
+		return;
+	printf("Name: ");
+	if (d->name)
+		printf("%s\n", d->name);
+	else
+		printf("(nil)\n");
+	printf("Age: ");
+	if (d->age)
+		printf("%f\n", d->age);
+	else
+		printf("(nil)\n");
+
+	printf("Owner: ");
+	if (d->owner)
+		printf("%s\n", d->owner);
+	else
+		printf("(nil)\n");
+
+}
