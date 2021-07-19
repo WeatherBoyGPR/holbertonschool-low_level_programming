23e02d141e9d51c006b83ca7f7d572f17ce466ee
Gino Rossi <gp.rossijr@gmail.com>
2021-07-19 17:55:09 +0000

    updated dog.h to comply with betty standards

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef SHEERHEARTATTACK
 #define SHEERHEARTATTACK
 
+/**
+ * struct dog - structure for a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ */
 typedef struct dog
 {
 	char *name;
