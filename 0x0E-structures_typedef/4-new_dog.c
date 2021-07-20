d350e7d89809a5e8544ea844f43c01b5437d110a
Gino Rossi <gp.rossijr@gmail.com>
2021-07-20 02:07:40 +0000

    updated 4-new_dog.c alongside dog.h headerfile

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,9 +19,45 @@ dog_t *new_dog(char *name, float age, char *owner)
 	pup = malloc(sizeof(dog_t));
 	if (!pup)
 		return (NULL);
-	pup->name = name;
-	pup->age = age;
-	pup->owner = owner;
 
+	pup->name = malloc(sizeof(name));
+	if (pup->name == NULL)
+	{
+		free(pup);
+		return (NULL);
+	}
+	pup->owner = malloc(sizeof(owner));
+	if (pup->owner == NULL)
+	{
+		free(pup->name);
+		free(pup);
+		return (NULL);
+	}
+	pup->age = age;
+	_strcpy(pup->name, name);
+	_strcpy(pup->owner, owner);
 	return (pup);
+}
+
+/**
+ * _strcpy - will copy a string to a destination buffer
+ * @dest: destination string
+ * @src: string to be copied from
+ *
+ * Return: pointer to dest string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	if (!(dest) || !(src))
+		return (0);
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		*(dest + i) = src[i];
+	}
+		*(dest + i) = '\0';
+
+	return (dest);
 }
