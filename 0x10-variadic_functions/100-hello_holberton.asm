8a0d2f85c66d6d65382a3b813e7f613ccc619673
Gino Rossi <gp.rossijr@gmail.com>
2021-07-23 18:24:02 +0000

    updated 100-hello_holberton.asm

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,17 +1,14 @@
 section .data
-screenmessage db "Hello, Holberton",10,0
-len equ $ - screenmessage
+	screenmessage db "Hello, Holberton",10,0
+	len equ $ - screenmessage
 
 section .text
-global main
+	global main
 
 main:
-mov eax, 4
-mov ebx, 1
-mov ecx, screenmessage
-mov edx, len
-int 0x80
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, screenmessage
+	mov edx, len
+	int 0x80
 
-mov eax, 1
-xor ebx, ebx
-int 0x80
