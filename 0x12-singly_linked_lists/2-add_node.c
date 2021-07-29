cb178fc976c38ff092768423d01f1957485c3c7d
Gino Rossi <gp.rossijr@gmail.com>
2021-07-29 21:55:46 +0000

    updated 2-add_node.c to account for NULL edge cases

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,20 +10,22 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newh = NULL;
-	char *s;
-	int len;
+	char *s = NULL;
+	int len = 0;
 
 	newh = malloc(sizeof(list_t));
 	if (newh == NULL)
 		return (NULL);
 
-	s = strdup(str);
-	if (s == NULL)
-		return (NULL);
+	if (str != NULL)
+	{
+		s = strdup(str);
+		if (s == NULL)
+			return (NULL);
 
-	for (len = 0; s[len] != '\0'; len++)
-		continue;
-
+		while (s[len] != '\0')
+			len++;
+	}
 	newh->str = s;
 	newh->len = (len);
 	newh->next = *head;
