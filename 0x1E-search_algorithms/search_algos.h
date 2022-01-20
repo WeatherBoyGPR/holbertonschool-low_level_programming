0f928ac3bcef41ca3b0c61a2e7fe580531c6e13d
WeatherBoyGPR <gp.rossijr@gmail.com>
2022-01-20 13:45:27 +0000

    added header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef HANGFIRE
+#define HANGFIRE
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+
+#endif
